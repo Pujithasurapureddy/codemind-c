34e045e365d3ca1d53ff85ae9e1e2e94716a96cd
Pujithasurapureddy <119917381+Pujithasurapureddy@users.noreply.github.com>
2023-05-23 14:37:15 +0530

    #include <stdio.h> int main() {     int i, j, rows;     scanf("%d", &rows);     for(i=1; i<=rows; i++)     {         for(j=1; j<=rows-i; j++)         {             printf(" ");         }         for(j=1; j<=rows; j++)         {             if(i==1 || i==rows || j==1 || j==rows)                 printf("*");             else                 printf(" ");         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for (i=1;i<=n;i++)
+    int i, j, rows;
+    scanf("%d", &rows);
+    for(i=1; i<=rows; i++)
     {
-        for (j=1;j<=i;j++)
+        for(j=1; j<=rows-i; j++)
         {
-            printf("%d",j);
+            printf(" ");
+        }
+        for(j=1; j<=rows; j++)
+        {
+            if(i==1 || i==rows || j==1 || j==rows)
+                printf("*");
+            else
+                printf(" ");
         }
         printf("
 ");
     }
+    return 0;
 }
